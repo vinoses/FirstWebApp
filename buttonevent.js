336d5007588f6af55cd2e73a49c76f373a554f23
vinoses <vinoses@users.noreply.github.com>
2019-05-19 09:14:52 -0600

    improved intuitive response, additional restructuring     Updated CSS3 layout with sections and animations

--- buttonevent.js
+++ buttonevent.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {  
   $("button").click(function() {
     var id = $(event.srcElement).attr('id');
-    var app = false;
-    //$("h2").text("Also consider: async / await, async control flow," + 
-   //   "Promise.all, Destructuring & default values, Truthy & falsy values," +
-   //   "Optional chaining,Class properties & binding, parcel bundling and transpiling");
-        
+    var app = false;  
+
     //getJSON parses json into objects
     $.getJSON("https://api.myjson.com/bins/9j806", function( data ) {
         createTable(data);      
     });
 
-    //create from listing on functions.json on remote server
+    //create objects of functions.json on remote server
     function createTable( data ) {
       var count = 0;
       var pieces;
+
+      //Button Event Handlers
       if(id === "js"){
         pieces = data.javscriptfunctions;
+        if($("table").is(':visible')){ 
+          $("h1").text("Javascript 80/20");
+          $("h2").text("Also consider: async / await, async control flow," + 
+          "Promise.all, Destructuring & default values, Truthy & falsy values," +
+          "Optional chaining,Class properties & binding, parcel bundling and transpiling");    
+        }
       } else if (id === "jq") {
         pieces = data.jqueryfunctions;
-      }
+        if($("table").is(':visible')){         
+          $("h1").text("jQuery 80/20");
+          $("h2").text("")
+               .append("<div> <a href=\"https://github.com/nefe/You-Dont-Need-jQuery\">but... You Dont Need jQuery</a> </div>");
+        }
+      };
       
+      //Table Creation
       $.each(pieces, function(key, val) {
         $("#table").append(
               "<tr id=tr" + count + ">" + 
--- general.js
+++ general.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {  
   var caller = 0;
   Cookies.set("caller","g");
+  
+  //general Button Event Handler
   $("button").click(function() {
     $("#table").html("");    
     $("#table").append("<tr>"+"<td>" + "Function" + "</td><td>" + "Description" +"</td></tr>");
@@ -8,12 +10,36 @@ $(document).ready(function() {
     var id = $(event.srcElement).attr('id');
     Cookies.set("reciever", id);
     var c = Cookies.get("caller");
-    if(c !== id) { 
+    if(c !== id) { 	
       $("table").show(); 
     } else { 
       $("table").toggle(); 
+      if($("table").not(':visible')) {
+      	trigger();
+      }
     }
     Cookies.set("caller",id);
+     if($("table").is(':visible')){
+  		$("#foot").css("height", "660px");
+  	} else {
+  		$("#foot").css("height", "860px");
+  	}
 
-  });  
+    dragonDance();
+
+  }); 
+
+
+  //Dragon Image Resizing
+  function dragonDance(){
+
+  } 
+
+  //Text Updates for no tables shown
+  function trigger() {
+  	
+    $("h1").text("Hey Sexy!");
+    $("h2").text("Good to see you again!");
+  };
+
 });
